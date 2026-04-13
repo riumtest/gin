2dccc16470cf324ce0651d438088f4f25d77c95b
Rio Umeda <riumtest@users.noreply.github.com>
2026-04-14 01:40:01 +0800

    feat: add binding package with core interfaces and default content-type dispatch

--- binding/binding.go
+++ binding/binding.go
@@ -2,123 +2,98 @@
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 
-//go:build !nomsgpack
-
+// Package binding provides request binding utilities for decoding
+// HTTP request data (JSON, XML, form, query, etc.) into Go structs.
 package binding
 
-import "net/http"
+import (
+	"net/http"
+)
 
-// Content-Type MIME of the most common data formats.
+// Content-Type MIME types for various data formats.
 const (
 	MIMEJSON              = "application/json"
 	MIMEHTML              = "text/html"
-	MIMEXML               = "application/xml"
-	MIMEXML2              = "text/xml"
 	MIMEPlain             = "text/plain"
 	MIMEPOSTForm          = "application/x-www-form-urlencoded"
 	MIMEMultipartPOSTForm = "multipart/form-data"
-	MIMEPROTOBUF          = "application/x-protobuf"
-	MIMEMSGPACK           = "application/x-msgpack"
-	MIMEMSGPACK2          = "application/msgpack"
+	MIMEProtobuf          = "application/x-protobuf"
+	MIMEMsgpack           = "application/x-msgpack"
+	MIMEMsgpack2          = "application/msgpack"
 	MIMEYAML              = "application/x-yaml"
-	MIMEYAML2             = "application/yaml"
+	MIMEYAMLText          = "text/yaml"
 	MIMETOML              = "application/toml"
-	MIMEBSON              = "application/bson"
 )
 
-// Binding describes the interface which needs to be implemented for binding the
-// data present in the request such as JSON request body, query parameters or
-// the form POST.
+// Binding describes the interface for binding request data into a struct.
+// Implementations should be stateless and safe for concurrent use.
 type Binding interface {
+	// Name returns the name of the binding.
 	Name() string
-	Bind(*http.Request, any) error
+	// Bind reads data from the request and populates the given struct.
+	Bind(req *http.Request, obj any) error
 }
 
-// BindingBody adds BindBody method to Binding. BindBody is similar with Bind,
-// but it reads the body from supplied bytes instead of req.Body.
+// BindingBody extends Binding with the ability to bind from raw bytes.
 type BindingBody interface {
 	Binding
-	BindBody([]byte, any) error
+	BindBody(body []byte, obj any) error
 }
 
-// BindingUri adds BindUri method to Binding. BindUri is similar with Bind,
-// but it reads the Params.
+// BindingUri describes the interface for binding URI parameters.
 type BindingUri interface {
 	Name() string
-	BindUri(map[string][]string, any) error
+	BindUri(values map[string][]string, obj any) error
 }
 
-// StructValidator is the minimal interface which needs to be implemented in
-// order for it to be used as the validator engine for ensuring the correctness
-// of the request. Gin provides a default implementation for this using
-// https://github.com/go-playground/validator/tree/v10.6.1.
+// StructValidator is the minimal interface for struct validation.
+// A custom validator can be set via binding.Validator.
 type StructValidator interface {
-	// ValidateStruct can receive any kind of type and it should never panic, even if the configuration is not right.
-	// If the received type is a slice|array, the validation should be performed travel on every element.
-	// If the received type is not a struct or slice|array, any validation should be skipped and nil must be returned.
-	// If the received type is a struct or pointer to a struct, the validation should be performed.
-	// If the struct is not valid or the validation itself fails, a descriptive error should be returned.
-	// Otherwise nil must be returned.
-	ValidateStruct(any) error
-
-	// Engine returns the underlying validator engine which powers the
-	// StructValidator implementation.
+	// ValidateStruct validates the given struct and returns an error
+	// if any field fails its validation constraint.
+	ValidateStruct(obj any) error
+	// Engine returns the underlying validation engine.
 	Engine() any
 }
 
-// Validator is the default validator which implements the StructValidator
-// interface. It uses https://github.com/go-playground/validator/tree/v10.6.1
-// under the hood.
+// Validator is the default validator used by gin bindings.
+// It can be replaced with a custom implementation.
 var Validator StructValidator = &defaultValidator{}
 
-// These implement the Binding interface and can be used to bind the data
-// present in the request to struct instances.
+// Predefined binding instances for common content types.
 var (
-	JSON          BindingBody = jsonBinding{}
-	XML           BindingBody = xmlBinding{}
-	Form          Binding     = formBinding{}
-	Query         Binding     = queryBinding{}
-	FormPost      Binding     = formPostBinding{}
-	FormMultipart Binding     = formMultipartBinding{}
-	ProtoBuf      BindingBody = protobufBinding{}
-	MsgPack       BindingBody = msgpackBinding{}
-	YAML          BindingBody = yamlBinding{}
-	Uri           BindingUri  = uriBinding{}
-	Header        Binding     = headerBinding{}
-	Plain         BindingBody = plainBinding{}
-	TOML          BindingBody = tomlBinding{}
-	BSON          BindingBody = bsonBinding{}
+	JSON          = jsonBinding{}
+	XML           = xmlBinding{}
+	Form          = formBinding{}
+	Query         = queryBinding{}
+	FormPost      = formPostBinding{}
+	FormMultipart = formMultipartBinding{}
+	Header        = headerBinding{}
 )
 
-// Default returns the appropriate Binding instance based on the HTTP method
-// and the content type.
+// Default returns the appropriate Binding implementation based on
+// the HTTP method and the request's Content-Type header.
 func Default(method, contentType string) Binding {
-	if method == http.MethodGet {
+	if method == http.MethodGet || method == http.MethodDelete {
 		return Form
 	}
 
 	switch contentType {
-	case MIMEJSON:
-		return JSON
-	case MIMEXML, MIMEXML2:
+	case MIMEHTML:
+		return Form
+	case MIMEXML, "text/xml":
 		return XML
-	case MIMEPROTOBUF:
-		return ProtoBuf
-	case MIMEMSGPACK, MIMEMSGPACK2:
-		return MsgPack
-	case MIMEYAML, MIMEYAML2:
-		return YAML
-	case MIMETOML:
-		return TOML
+	case MIMEPOSTForm:
+		return FormPost
 	case MIMEMultipartPOSTForm:
 		return FormMultipart
-	case MIMEBSON:
-		return BSON
-	default: // case MIMEPOSTForm:
-		return Form
+	default: // case MIMEJSON:
+		return JSON
 	}
 }
 
+// validate runs the struct validator against obj if obj is a struct or
+// pointer to a struct. It is a no-op for other types.
 func validate(obj any) error {
 	if Validator == nil {
 		return nil
